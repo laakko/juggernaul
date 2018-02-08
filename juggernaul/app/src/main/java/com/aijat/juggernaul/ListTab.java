bcd5d5a506d7f84fbeb2412fcb7282456acd0976
Jukka Laakko <jukka.laakko@aalto.fi>
2018-02-08 15:19:39 +0200

    Added custom layout file for a task item. Added a function for creating a list with the custom items.

--- juggernaul/app/src/main/java/com/aijat/juggernaul/ListTab.java
+++ juggernaul/app/src/main/java/com/aijat/juggernaul/ListTab.java
@@ -1,5 +1,6 @@
     package com.aijat.juggernaul;
 
+    import android.content.Context;
     import android.content.Intent;
     import android.os.Bundle;
     import android.support.design.widget.FloatingActionButton;
@@ -13,8 +14,10 @@
     import android.widget.ArrayAdapter;
     import android.widget.Button;
     import android.widget.EditText;
+    import android.widget.ImageView;
     import android.widget.ListView;
     import android.widget.PopupWindow;
+    import android.widget.TextView;
 
     import java.util.ArrayList;
     import java.util.Arrays;
@@ -34,6 +37,7 @@
             View view = inflater.inflate(R.layout.fragment_list, container, false);
 
 
+
             // Initialize list view
             list = (ListView) view.findViewById(R.id.taskList);
 
@@ -50,8 +54,6 @@
             list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                    // TODO: Figure out how to get all the item contents as parameter into the TaskActivity class
                     temp_title = (String) adapterView.getItemAtPosition(i);
                     startActivity(new Intent(getActivity(), TaskActivity.class));
                 }
@@ -67,7 +69,9 @@
                     LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(LAYOUT_INFLATER_SERVICE);
                     View layout = inflater.inflate(R.layout.add_task,
                             (ViewGroup) view.findViewById(R.id.tab1_main_layout));
-                    popup = new PopupWindow(layout, 1080, 900, true);
+
+
+                    popup = new PopupWindow(layout, 1080, 900, true); // TODO: make it scalable, now its for full hd screens only
                     //popup.setAnimationStyle(R.anim.fadein);
                     popup.showAtLocation(layout, Gravity.TOP, 0, 100);
 
@@ -79,8 +83,10 @@
                         @Override
                         public void onClick(View view) {
 
-                            // TODO add deadline, priority, and other information to list/array as well
                             items_list.add(txtPop.getText().toString());
+
+                            // Create json -> add to json list
+
                             arrayAdapter.notifyDataSetChanged();
 
                         }
@@ -89,7 +95,61 @@
 
                 }
             });
+
+
             return view;
 
         }
+
+
+
+
+        // This is for creating a custom list item (e.g. our task-item)
+        public class listview_adapter extends ArrayAdapter {
+            private Context context;
+            private LayoutInflater inflater;
+
+            private String[] json_task;
+
+            // Constructor, adapter takes JSON of a task as input
+            public listview_adapter(Context context, String[] json_task) {
+                super(context, R.layout.layout_task); // Use the custom layout_task xml
+                this.context = context;
+                this.json_task = json_task;
+
+                inflater = LayoutInflater.from(context); // Inflate the task
+            }
+
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if (null == convertView) {
+                    convertView = inflater.inflate(R.layout.layout_task, parent, false);
+                }
+
+                // Set task parameters here
+
+                // Set title
+                TextView listTaskTitle = (TextView) convertView.findViewById(R.id.listTaskTitle);
+
+                // Set deadline
+                TextView listTaskDeadline = (TextView) convertView.findViewById(R.id.listTaskDeadline);
+
+                // Set category
+                TextView listTaskCategory = (TextView) convertView.findViewById(R.id.listTaskCategory);
+
+                // Set status image
+                ImageView listTaskStatus = (ImageView) convertView.findViewById(R.id.listTaskStatus);
+
+                return convertView;
+
+            }
+
+        }
+
+
+
     }
+
+
+

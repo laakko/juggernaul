da2874fe499f12681aeac24a3d5e0fdeae07e68c
Jukka Laakko <jukka.laakko@aalto.fi>
2018-02-22 18:02:22 +0200

    Added UI for single task view. Added skeleton of add members view, opened via share button.

---
+++ juggernaul/app/src/main/java/com/aijat/juggernaul/ShareActivity.java
@@ -0,0 +1,21 @@
+package com.aijat.juggernaul;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+
+
+        // TODO Generate QR-code and display it in image view
+        // TODO get QR-code content (token) from backend server
+
+
+    }
+
+}
--- juggernaul/app/src/main/java/com/aijat/juggernaul/TaskActivity.java
+++ juggernaul/app/src/main/java/com/aijat/juggernaul/TaskActivity.java
@@ -1,24 +1,53 @@
 package com.aijat.juggernaul;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.github.clans.fab.FloatingActionButton;
 
 import static com.aijat.juggernaul.ListTab.test_title;
 
 public class TaskActivity extends AppCompatActivity {
 
     private TextView txtTaskTitle;
+    private View rect;
+    private TextView txtTaskDeadline;
+    private TextView txtTaskStatus;
+    private FloatingActionButton sharebutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
 
-        txtTaskTitle = (TextView)findViewById(R.id.txtTaskTitle);
+        txtTaskTitle = (TextView) findViewById(R.id.txtTaskTitle);
 
+        // Temp ----------------------
+        rect = (View) findViewById(R.id.rectPriority);
+        rect.setBackgroundColor(Color.rgb(137, 244, 66));
         txtTaskTitle.setText(test_title);
 
+        txtTaskDeadline = (TextView) findViewById(R.id.txtTaskDeadline);
+        txtTaskDeadline.setText("15 Days left");
+        txtTaskStatus = (TextView) findViewById(R.id.txtTaskStatus);
+        txtTaskStatus.setText("In progress");
 
+        // -----------------------------
+
+        sharebutton = (FloatingActionButton) findViewById(R.id.fab_share);
+        sharebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(TaskActivity.this, ShareActivity.class));
+
+            }
+        });
     }
+
 }

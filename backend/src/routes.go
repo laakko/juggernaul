8214fc17a547ec95d13f319687c1b55317457675
Juuso Jahnukainen <juuso.jahnukainen@aalto.fi>
2018-04-25 12:01:05 +0300

    Upload New File

---
+++ backend/src/routes.go
@@ -0,0 +1,51 @@
+package main
+
+import "net/http"
+
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+var routes = Routes{
+	Route{
+		"Index",
+		"GET",
+		"/",
+		Index,
+	},
+	Route{
+		"TodoIndex",
+		"GET",
+		"/todos",
+		TodoIndex,
+	},
+	Route{
+		"TodoCreate",
+		"POST",
+		"/todos",
+		TodoCreate,
+	},
+	Route{
+		"TodoShow",
+		"GET",
+		"/todos/{todoId}",
+		TodoShow,
+	},
+	Route{
+		"CreateGroup",
+		"POST",
+		"/groups/create",
+		CreateGroup,
+	},
+	Route{
+		"JoinGroup",
+		"POST",
+		"/groups/{joinerToken}",
+		JoinGroup,
+	},
+}
